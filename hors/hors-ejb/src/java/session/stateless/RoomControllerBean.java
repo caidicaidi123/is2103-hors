eacc77a393c8dafa10e008d19868d0b3d23de01c
caidicaidi123 <caidicaidi123@gmail.com>
2018-11-12 22:59:13 +0800

    create new room type

--- hors/hors-ejb/src/java/session/stateless/RoomControllerBean.java
+++ hors/hors-ejb/src/java/session/stateless/RoomControllerBean.java
@@ -5,7 +5,11 @@
  */
 package session.stateless;
 
+import entity.RoomType;
+import java.math.BigDecimal;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -14,6 +18,16 @@ import javax.ejb.Stateless;
 @Stateless
 public class RoomControllerBean implements RoomControllerBeanRemote {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    @PersistenceContext(unitName = "hors-ejbPU")
+    private EntityManager em;
+
+    public String createNewRoomType(String typeName, String description, BigDecimal rate) {
+        RoomType roomType = new RoomType(typeName, description, rate);
+        try {
+            em.persist(roomType);
+            return "New room type created successfully";
+        } catch (Exception e) {
+            return e.toString();
+        }
+    }
 }
